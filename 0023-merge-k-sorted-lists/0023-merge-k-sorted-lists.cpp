e2b75530af5810ad8f359019b34f74f908668675
Vansh Kumar <203538506+vvanshkkumar@users.noreply.github.com>
2026-07-20 15:02:06 +0530

    Time: 195 ms (5.12%), Space: 18.7 MB (38.53%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+   ListNode* merge(ListNode* head1, ListNode* head2){
+
+      ListNode* dummy = new ListNode(-1);
+      ListNode* temp = dummy;
+      ListNode* curr1 = head1;
+      ListNode* curr2 = head2;
+
+      while(curr1!=NULL && curr2!=NULL){
+
+         if(curr1->val <= curr2->val){
+
+            ListNode* future = curr1->next;
+
+            temp->next = curr1;
+            temp = temp->next;
+
+            curr1 = future;
+         }
+         else {
+            ListNode* future = curr2->next;
+
+            temp->next = curr2;
+            temp = temp->next;
+
+            curr2 = future;
+         }
+      }
+
+      while(curr1!=NULL){
+        ListNode* future = curr1->next;
+        temp->next = curr1;
+        temp = temp->next;
+        curr1 = future;
+      }
+
+      while(curr2!=NULL){
+        ListNode* future = curr2->next;
+        temp->next = curr2;
+        temp = temp->next;
+        curr2 = future;
+      }
+
+      ListNode* head = dummy->next;
+      delete dummy;
+
+      return head;
+   }
+
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        if(lists.size() == 0) return NULL;
+        
+        ListNode* prev = lists[0];
+
+        for(int i=1;i<lists.size();i++){
+
+            ListNode* curr = lists[i];
+
+            prev = merge(prev, curr);
+        }
+
+        return prev;
+    }
+};
