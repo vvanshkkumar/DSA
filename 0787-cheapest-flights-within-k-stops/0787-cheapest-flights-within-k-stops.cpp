80adc72ae1d40633ba35672a8f9a0d53ba85af4d
Vansh Kumar <203538506+vvanshkkumar@users.noreply.github.com>
2026-08-24 17:17:05 +0530

    Time: 167 ms (5%), Space: 54 MB (5%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<int> dist(n,INT_MAX);
+        dist[src] = 0;
+        
+        
+        for(int i=0;i<k+1;i++){
+
+            vector<int> temp = dist;
+            
+            for(auto flight : flights) {
+    int u = flight[0];
+    int v = flight[1];
+    int price = flight[2];
+               
+               if(dist[u]!=INT_MAX)
+              temp[v] = min(dist[u] + price ,temp[v]);
+
+            }
+            dist = temp;
+        }
+
+        return dist[dst]==INT_MAX?-1:dist[dst];
+    }
+};
